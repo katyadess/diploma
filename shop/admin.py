e975323a104fe7cedfb20bb60297501d3c0c4441
katyadess <trynovakatya345@gmail.com>
2024-08-14 16:49:34 +0300

    register models and migrate changes

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'parent']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Brand, BrandAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'is_available', 'created_at', 'updated_at']
+    list_filter = ['is_available', 'created_at', 'updated_at']
+    list_editable = ['price', 'stock', 'is_available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'rating']
+admin.site.register(ProductReview, ProductReviewAdmin)
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city', 'street']
+admin.site.register(Address, AddressAdmin)
