d83f5487cbe9eb6b8800fb9ebe20e7f7714c5bf9
katyadess <trynovakatya345@gmail.com>
2024-09-01 19:07:50 +0300

    clearing notification instances after sending email

--- shop/signals.py
+++ shop/signals.py
@@ -13,12 +13,14 @@ def check_stock_and_notify(sender, instance, **kwargs):
         )
         
         for n in notifications:
-            send_mail(
-                'Product back in stock!',
-                f'The product {instance.name} is back in stock!',
-                settings.EMAIL_HOST_USER,
-                [n.user.email],
-                fail_silently=False,
-            )
-            n.notified = True
-            n.save()
+            try:
+                send_mail(
+                    'Product back in stock!',
+                    f'The product {instance.name} is back in stock!',
+                    settings.EMAIL_HOST_USER,
+                    [n.user.email],
+                    fail_silently=False,
+                )
+                n.delete()
+            except Exception as e:
+                print(f"Failed to send email to {n.user.email}: {e}")
